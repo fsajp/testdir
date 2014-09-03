14e71cdbd1c051ac16679637f3646e360068bd07
fenny <sanyoto.fenny@nims.go.jp>
2014-09-03 10:26:12 +0900

    Adding Addressbook php

---
+++ tempt/addressbook.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+
+<title> Address Book </title>
+</head>
+
+<body>
+
+<?php
+
+try
+{
+//open the database
+$db = new PDO('sqlite:home/fenny/public_html/tempt/addressbook.db');
+
+
+
+
+
+}
+</body>
+</html>
+
+
